d91974457071b392a7d18b1c31e2e53870afb20e
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-04-05 16:50:45 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,9 +5,14 @@ function Navigation() {
   
   return (
     <div className='mt-2'>
+    
+        {/* Creating link for statistics */}
       <NavLink to="/statistics" className='text-gray-600 mr-5 ml-20 stats'>Statistics</NavLink>
+         {/* Creating link for overview */}
       <NavLink to="/" className='text-gray-600 mx-5'>Overview</NavLink>
+          {/* Creating link for dashboard */}
       <NavLink to="/dashboard" className='text-gray-600 mx-5'>Dashboard</NavLink>
+               {/* Creating link for analytics */}
       <NavLink to="/analytics" className='text-gray-600 ml-5  mr-20 '>Analytics</NavLink>
     </div>
   )
