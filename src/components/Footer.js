d1d2c61bca9e4b60b8a40f8b9baf394a11f629a4
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-04-05 16:47:54 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
+
+        {/*Creating Footer section */}
     <div className='relative'>
       <p className='absolute left-12 p-1.5'>ORION DATA VISUALISATION</p>
       <p className='absolute right-12 p-1.5'>2022</p>
