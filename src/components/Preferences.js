f23e39d31734c3414ba7a81083b715c5a2646359
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-04-05 16:52:42 +0530

    Update Preferences.js

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Preferences() {
   return (
     <div className='mt-2'>
+       {/* user */}
       <i className="fa fa-user text-gray-500 ml-20   mr-3"></i>
+   {/* Settings */}
       <i className="fa fa-sliders text-gray-500 mx-5"></i>
     </div>
   )
