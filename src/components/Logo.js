b876c63846555b58487c3887a34cc8adc2562a43
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-04-05 16:49:03 +0530

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Logo() {
     return (
     <div className='flex'>
+            {/* Fetching logo of the company */}
       <img className="h-10 ml-10 mb-1" src="https://www.thewasserstoff.com/images/wstf-logo.svg" alt="logo" />
+              {/* Writing name of the company */}
       <h5 className='pt-1.5 font-medium ml-2 mr-20'>WASSERSTOFF</h5>
     </div>
     
